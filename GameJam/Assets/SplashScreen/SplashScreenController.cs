84688b0a4503ce5c4a7e17139eb16ef77a529c87
Rob Garner <rgarner011235@gmail.com>
2015-09-12 14:11:09 -0600

    #15 -done #17 -done created splash screen and script to start game

---
+++ GameJam/Assets/SplashScreen/SplashScreenController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class SplashScreenController : MonoBehaviour {
+
+    public Canvas gameScreen;
+    public Text status;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        this.gameObject.SetActive(false);
+        gameScreen.gameObject.SetActive(true);
+        status.text = "Clicked!";
+    }
+
+}
